ab62e9feaf574c06ff1e49c6eed9cfe6b09c4dab
Raghavnadiminti <raghavnadiminti@gmail.com>
2025-10-05 21:06:07 +0530

    basic folder structure created to collaborate

---
+++ public/data.ts
@@ -0,0 +1,99 @@
+export interface UserData {
+  "User Name": string;
+  "User Email": string;
+  "Google Cloud Skill": string;
+  "Profile URL Status": string;
+  "Access Code Redeemed": string;
+  "All Skill Badges": string;
+  "# of Skill Badges Completed": number;
+  "Names of Completed Skill Badges": string;
+  "# of Arcade Games Completed": number;
+  "Names of Completed Arcade Games": string;
+}
+
+export const mockData: UserData[] = [
+  {
+    "User Name": "Raghavendra",
+    "User Email": "raghave999gskb@gmail.com",
+    "Google Cloud Skill": "https://www.cloudskillsboost.google/",
+    "Profile URL Status": "All Good",
+    "Access Code Redeemed": "Yes",
+    "All Skill Badges": "No",
+    "# of Skill Badges Completed": 0,
+    "Names of Completed Skill Badges": "",
+    "# of Arcade Games Completed": 0,
+    "Names of Completed Arcade Games": ""
+  },
+  {
+    "User Name": "S.V.KISHORE REDDY",
+    "User Email": "kishore56234@gmail.com",
+    "Google Cloud Skill": "https://www.cloudskillsboost.google/",
+    "Profile URL Status": "All Good",
+    "Access Code Redeemed": "Yes",
+    "All Skill Badges": "No",
+    "# of Skill Badges Completed": 0,
+    "Names of Completed Skill Badges": "",
+    "# of Arcade Games Completed": 0,
+    "Names of Completed Arcade Games": ""
+  },
+  {
+    "User Name": "Alen Alexander",
+    "User Email": "alenalexander001@gmail.com",
+    "Google Cloud Skill": "https://www.cloudskillsboost.google/",
+    "Profile URL Status": "All Good",
+    "Access Code Redeemed": "Yes",
+    "All Skill Badges": "No",
+    "# of Skill Badges Completed": 8,
+    "Names of Completed Skill Badges": "The Basics of Google Cloud",
+    "# of Arcade Games Completed": 0,
+    "Names of Completed Arcade Games": ""
+  },
+  {
+    "User Name": "Pakalapati Deekshith",
+    "User Email": "deekshithpakalapati@gmail.com",
+    "Google Cloud Skill": "https://www.cloudskillsboost.google/",
+    "Profile URL Status": "All Good",
+    "Access Code Redeemed": "No",
+    "All Skill Badges": "No",
+    "# of Skill Badges Completed": 0,
+    "Names of Completed Skill Badges": "",
+    "# of Arcade Games Completed": 0,
+    "Names of Completed Arcade Games": ""
+  },
+  {
+    "User Name": "Gandi Komali",
+    "User Email": "gandikomali0410@gmail.com",
+    "Google Cloud Skill": "https://www.cloudskillsboost.google/",
+    "Profile URL Status": "All Good",
+    "Access Code Redeemed": "No",
+    "All Skill Badges": "No",
+    "# of Skill Badges Completed": 0,
+    "Names of Completed Skill Badges": "",
+    "# of Arcade Games Completed": 0,
+    "Names of Completed Arcade Games": ""
+  },
+  {
+    "User Name": "Murali Lakshmi Sai",
+    "User Email": "saipatnala248@gmail.com",
+    "Google Cloud Skill": "https://www.cloudskillsboost.google/",
+    "Profile URL Status": "All Good",
+    "Access Code Redeemed": "No",
+    "All Skill Badges": "No",
+    "# of Skill Badges Completed": 0,
+    "Names of Completed Skill Badges": "",
+    "# of Arcade Games Completed": 0,
+    "Names of Completed Arcade Games": ""
+  },
+  {
+    "User Name": "DUDDU RAVI KUMAR",
+    "User Email": "rkumarduddu@gmail.com",
+    "Google Cloud Skill": "https://www.cloudskillsboost.google/",
+    "Profile URL Status": "All Good",
+    "Access Code Redeemed": "Yes",
+    "All Skill Badges": "No",
+    "# of Skill Badges Completed": 1,
+    "Names of Completed Skill Badges": "The Basics of Google Cloud",
+    "# of Arcade Games Completed": 0,
+    "Names of Completed Arcade Games": ""
+  }
+];
